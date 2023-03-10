06d59a7145630eb44c0e6cfef41dd5a6b20b0114
hrayr <developer.hovakimyan@gmail.com>
2023-03-10 20:30:31 +0400

    Use another API for taking screenshots
    chrome.tabs.captureVisibleTab method is used for
    taking screenshots as the desktop capture API
    required users permission for every screenshot.

--- scripts/background.js
+++ scripts/background.js
@@ -1,9 +1,7 @@
 chrome.runtime.onMessage.addListener((message, sender) => {
     if (sender.tab && message.event === "CLICK_ON_PAGE") {
-        chrome.desktopCapture.chooseDesktopMedia(["screen"], sender.tab, (streamId) => {
-            if (streamId && streamId.length) {
-                chrome.tabs.sendMessage(sender.tab.id, {streamId, success: true});
-            }
-        })
+        chrome.tabs.captureVisibleTab(null, {}, function (image) {
+            chrome.tabs.sendMessage(sender.tab.id, {image, success: true});
+        });
     }
 });
--- scripts/content-script.js
+++ scripts/content-script.js
@@ -9,36 +9,11 @@ const onDocumentClick = function (event) {
 }
 
 chrome.runtime.onMessage.addListener((message, sender) => {
-    if (message?.streamId) {
-        let track, canvas
-        navigator.mediaDevices.getUserMedia({
-            video: {
-                mandatory: {
-                    chromeMediaSource: 'desktop',
-                    chromeMediaSourceId: message.streamId
-                },
-            }
-        }).then((stream) => {
-            track = stream.getVideoTracks()[0];
-            const imageCapture = new ImageCapture(track);
-            return imageCapture.grabFrame();
-        }).then((bitmap) => {
-            track.stop();
-            canvas = document.createElement('canvas');
-            canvas.width = bitmap.width;
-            canvas.height = bitmap.height;
-            let context = canvas.getContext('2d');
-            context.drawImage(bitmap, 0, 0, bitmap.width, bitmap.height);
-
-            return canvas.toDataURL();
-        }).then((url) => {
-            console.log('Screenshot taken');
-        }).catch((err) => {
-            console.error(err);
-        })
-    } else if (message.stopRecording) {
+    if (message.stopRecording) {
         document.getElementsByTagName('body')[0].removeEventListener('click', onDocumentClick);
     } else if (message.startRecording) {
         document.getElementsByTagName('body')[0].addEventListener('click', onDocumentClick);
+    } else if (message.image) {
+        console.log(message.image);
     }
 });
