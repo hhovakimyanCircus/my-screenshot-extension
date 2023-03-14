2d23654e35b02aaeb088b70ad8c288e1f6be278e
hrayr <developer.hovakimyan@gmail.com>
2023-03-15 01:45:40 +0400

    Update extension UI
    Bootstrap is used to update extension UI.

--- popup/index.js
+++ popup/index.js
@@ -1,13 +1,17 @@
 const startRecordingBtn = document.getElementById('startRecordingBtn');
 startRecordingBtn.addEventListener('click', function () {
     chrome.tabs.query({ active: true }).then((result) => {
-        chrome.tabs.sendMessage(result[0].id, {startRecording: true});
+        if (result?.[0]?.id) {
+            chrome.tabs.sendMessage(result[0].id, {startRecording: true});
+        }
     });
 })
 
 const stopRecordingBtn = document.getElementById('stopRecordingBtn');
 stopRecordingBtn.addEventListener('click', function () {
     chrome.tabs.query({ active: true }).then((result) => {
-        chrome.tabs.sendMessage(result[0].id, {stopRecording: true});
+        if (result?.[0]?.id) {
+            chrome.tabs.sendMessage(result[0].id, {stopRecording: true});
+        }
     });
 })
