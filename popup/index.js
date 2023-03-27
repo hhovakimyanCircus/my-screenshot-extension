e794bf1cf6f2475c78aa7527b9dd1882996b6d41
hrayr <developer.hovakimyan@gmail.com>
2023-03-27 23:57:26 +0400

    Implement recording timer

--- popup/index.js
+++ popup/index.js
@@ -6,8 +6,32 @@ const startRecordingBtn = document.getElementById('startRecordingBtn');
 const stopRecordingBtn = document.getElementById('stopRecordingBtn');
 const authenticateSection = document.getElementById('authenticateSection');
 const recordingSection = document.getElementById('recordingSection');
+const timer = document.getElementById('timer');
+let recordingTimerId = null;
 
-const startRecordingFunction = function () {
+const updateRecordingTimer = function (startTime) {
+    let currentDiffTimestamp = Math.ceil((Date.now() - startTime) / 1000);
+    let currentMinutes = Math.floor(currentDiffTimestamp / 60);
+    let currentSeconds = currentDiffTimestamp - (currentMinutes * 60);
+    timer.innerText = String(currentMinutes).padStart(2, '0') + ":" + String(currentSeconds).padStart(2, '0');
+}
+
+const startRecordingTimer = function () {
+    stopRecordingTimer();
+    updateRecordingTimer(recordingStartTime);
+    recordingTimerId = setInterval(() => {
+        updateRecordingTimer(recordingStartTime)
+    }, 1000);
+}
+
+const stopRecordingTimer = function () {
+    if (recordingTimerId) {
+        clearInterval(recordingTimerId);
+        recordingTimerId = null;
+    }
+}
+
+const startRecording = function () {
     chrome.tabs.query({ active: true }).then((result) => {
         if (result?.[0]?.id) {
             chrome.tabs.sendMessage(
@@ -19,21 +43,27 @@ const startRecordingFunction = function () {
                 }
             );
             startRecordingBtn.classList.add('hidden');
+            stopRecordingBtn.addEventListener('click', stopRecording);
             recordingSection.classList.remove('hidden');
+
             recordingStartTime = Date.now();
             chrome.storage.local.set({ recordingStartTime: recordingStartTime });
+            startRecordingTimer();
         }
     });
 };
 
-const stopRecordingFunction = function () {
+const stopRecording = function () {
     chrome.tabs.query({ active: true }).then((result) => {
         if (result?.[0]?.id) {
             chrome.tabs.sendMessage(result[0].id, {stopRecording: true});
+            startRecordingBtn.addEventListener('click', startRecording);
             startRecordingBtn.classList.remove('hidden');
             recordingSection.classList.add('hidden');
+
             recordingStartTime = null;
             chrome.storage.local.set({ recordingStartTime: null });
+            stopRecordingTimer();
         }
     });
 };
@@ -43,10 +73,11 @@ const onSignIn = function (authenticationData) {
     currentUseId = authenticationData.id;
     if (recordingStartTime) {
         recordingSection.classList.remove('hidden');
-        stopRecordingBtn.addEventListener('click', stopRecordingFunction);
+        stopRecordingBtn.addEventListener('click', stopRecording);
+        startRecordingTimer();
     } else {
         startRecordingBtn.classList.remove('hidden');
-        startRecordingBtn.addEventListener('click', startRecordingFunction);
+        startRecordingBtn.addEventListener('click', startRecording);
     }
 
     authenticateSection.classList.add('hidden');
@@ -58,14 +89,15 @@ const onSignOut = function () {
 
     if (recordingStartTime) {
         recordingSection.classList.add('hidden');
-        stopRecordingBtn.removeEventListener('click', stopRecordingFunction);
+        stopRecordingBtn.removeEventListener('click', stopRecording);
     } else {
         startRecordingBtn.classList.add('hidden');
-        startRecordingBtn.removeEventListener('click', startRecordingFunction);
+        startRecordingBtn.removeEventListener('click', startRecording);
     }
 
     authenticateSection.classList.remove('hidden');
     chrome.storage.local.set({ recordingStartTime: null });
+    stopRecordingTimer();
 }
 
 chrome.storage.local.get(["user", "recordingStartTime"]).then((result) => {
