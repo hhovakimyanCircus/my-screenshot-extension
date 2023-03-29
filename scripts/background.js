7340957e214677db260b463f65246b9bb3638f6d
hrayr <developer.hovakimyan@gmail.com>
2023-03-29 18:29:20 +0400

    Change Firebase db structure
    Firebase db structure is changed to have separate
    hierarchy for each session of each unique user.

--- scripts/background.js
+++ scripts/background.js
@@ -1,11 +1,6 @@
 const firebaseConfig = {
     apiKey: "AIzaSyBTVcYHhjfq3QCa0kAqsV7b8q4NfLQiXho",
-    authDomain: "screenshoter-dfcd1.firebaseapp.com",
     databaseURL: "https://screenshoter-dfcd1-default-rtdb.firebaseio.com",
-    projectId: "screenshoter-dfcd1",
-    storageBucket: "screenshoter-dfcd1.appspot.com",
-    messagingSenderId: "957518554914",
-    appId: "1:957518554914:web:6b11bd5b963412a453277b",
 };
 
 let idToken;
@@ -42,12 +37,12 @@ const refreshIdToken = async (refreshToken) => {
     }
 }
 
-const insertIntoDatabase = async (refreshToken, userId, data) => {
+const insertIntoDatabase = async (refreshToken, userId, sessionId, data) => {
     if (!idToken) {
         idToken = await refreshIdToken(refreshToken);
     }
 
-    fetch(`${firebaseConfig.databaseURL}/users/${userId}/.json?auth=${idToken}`, {
+    fetch(`${firebaseConfig.databaseURL}/users/${userId}/${sessionId}/.json?auth=${idToken}`, {
         method: "POST",
         headers: {
             'Content-Type': 'application/json',
@@ -67,14 +62,14 @@ try {
                 insertIntoDatabase(
                     message.refreshToken,
                     message.userId,
+                    message.sessionId,
                     {
                         clickedElementName: message.data.elementName,
                         image: image,
                         url: sender.tab.url,
                         website: sender.tab.url.split('/')[2],
                         setupId: extensionId,
-                        date: new Date().toLocaleString(),
-                        sessionId: message.sessionId,
+                        timestamp: Date.now(),
                     }
                 )
             });
