9be9e97419509a375ad419f99d0ecf2dc6b23492
hrayr <developer.hovakimyan@gmail.com>
2023-04-19 20:46:15 +0400

    Add overlay to screen on recording start
    A code change is done to add an overlay to screen
    when start recording button is clicked. The overlay is
    removed in 3 seconds.

--- scripts/content-script.js
+++ scripts/content-script.js
@@ -14,6 +14,51 @@ const onDocumentClick = function (event, sessionId, userId, refreshToken) {
     });
 }
 
+const convertCssStylesToText = (styles) => {
+    return Object.keys(styles)
+        .reduce(
+            (accumulator, currentValue) => accumulator + `${currentValue}:${styles[currentValue]};`,
+            ''
+        )
+        .slice(0, -1);
+}
+
+const addOverlayToScreen = () => {
+    const overlayWindowStyles = {
+        background: 'rgba(0, 0, 0, .5)',
+        width: '100%',
+        height: '100%',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        'z-index': 100,
+        display: 'flex',
+        'justify-content': 'center',
+        'align-items': 'center',
+    };
+
+    const overlayWindowTextStyles = {
+        color: '#fff',
+        'font-size': '47px',
+        'font-weight': 700,
+    };
+
+    const overlayWindow = document.createElement('div');
+    overlayWindow.setAttribute('id', 'my-screenshot-overlay-window');
+    overlayWindow.setAttribute('style', convertCssStylesToText(overlayWindowStyles));
+
+    const overlayWindowText = document.createElement('span');
+    overlayWindowText.innerText = "Recording is starting 👌";
+    overlayWindowText.setAttribute('style', convertCssStylesToText(overlayWindowTextStyles))
+    overlayWindow.appendChild(overlayWindowText);
+
+    document.body.appendChild(overlayWindow);
+
+    setTimeout(() => {
+        overlayWindow.remove()
+    }, 3000)
+}
+
 window.addEventListener('MY_SCREENSHOTER_LOGIN', (event) => {
     chrome.storage.local.set({ user: {id: event.detail.userId, refreshToken: event.detail.refreshToken} });
 });
@@ -36,6 +81,8 @@ chrome.runtime.onMessage.addListener((message, sender) => {
 
         document.getElementsByTagName('body')[0].removeEventListener('click', onDocumentClick);
     } else if (message.startRecording) {
+        addOverlayToScreen();
+
         document.getElementsByTagName('body')[0]
             .addEventListener(
             'click',
