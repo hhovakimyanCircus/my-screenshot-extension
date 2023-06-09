8e01bf70ecedb04d2968bcc42ec03ef105cba6f7
Artak <artakom98@gmail.com>
2023-06-09 15:19:08 +0400

    Append detector element to dashboard body

---
+++ scripts/detector/index.js
@@ -0,0 +1,5 @@
+const detector = document.createElement('div')
+detector.setAttribute('id', 'flowl-screenshoter-extension-detector');
+detector.style.display = 'none';
+
+document.body.appendChild(detector);
